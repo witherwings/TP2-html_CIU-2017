814c84589335e63cc05cb35c0e0bc3a7c21b3cef
Lucas <lucas.wfh@gmail.com>
2017-05-22 22:10:55 -0300

    se expandio el modelo temporal

--- js/paisesApp.js
+++ js/paisesApp.js
@@ -6,15 +6,51 @@ app.controller('PaisesCtrl', function () {
 
 	this.paises = [{
 		"nombre": "Argentina",
+		"conexiones": ["Estados Unidos", "Mexico"],
+		"lugares": [
+			{
+				"nombre": "Embajada"
+				"occupant": {
+					"clue": "una pista"
+				}
+			}
+		],
 		"id" : 1
 	}, {
 		"nombre": "Estados Unidos",
+		"conexiones": ["Francia", "Mexico"],
+		"lugares": [
+			{
+				"nombre": "Libreria"
+				"occupant": {
+					"clue": "otra pista"
+				}
+			}
+		],
 		"id" : 2
 	}, {
 		"nombre": "Mexico",
+		"conexiones": ["Francia", "Argentina"],
+		"lugares": [
+			{
+				"nombre": "Club"
+				"occupant": {
+					"clue": "una pista mas del monton"
+				}
+			}
+		],
 		"id" : 3
 	}, {
 		"nombre": "Francia",
+		"conexiones": ["Estados Unidos", "Argentina"],
+		"lugares": [
+			{
+				"nombre": "Banco"
+				"occupant": {
+					"clue": "alguna pista (de tu hermana)"
+				}
+			}
+		],
 		"id" : 4
 	}];
 	
